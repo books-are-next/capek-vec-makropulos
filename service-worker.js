3db2db423e316756423143c17986ab8bcc49d22a
jan-martinek <honza.martinek@gmail.com>
2022-06-11 11:57:41 +0000

    Deploying to docs from @ books-are-next/capek-vec-makropulos@ee21709acdb0281f4665ec374faac7064acf16f1 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-f70842c';
+const CACHE = 'cache-ee21709';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
@@ -1019,7 +1019,7 @@ async function cache(context) {
         .open(CACHE)
         .then(cache => {
         console.log('[' + context + '] Opened cache');
-        return cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./promo.html","./vec_makropulos_005.html","./vec_makropulos_006.html","./vec_makropulos_007.html","./vec_makropulos_008.html","./vec_makropulos_009.html","./vec_makropulos_010.html","./vec_makropulos_011.html","./vec_makropulos_012.html","./vec_makropulos_013.html","./vec_makropulos_014.html","./vec_makropulos_015.html","./vec_makropulos_016.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./scripts/bundle.js","./resources/image001.jpg","./resources/image002.jpg","./resources/image003.png","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./template-images/circles.png","./style/style.min.css"]);
+        return cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./promo.html","./vec_makropulos_005.html","./vec_makropulos_006.html","./vec_makropulos_007.html","./vec_makropulos_008.html","./vec_makropulos_009.html","./vec_makropulos_010.html","./vec_makropulos_011.html","./vec_makropulos_012.html","./vec_makropulos_013.html","./vec_makropulos_014.html","./vec_makropulos_015.html","./vec_makropulos_016.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/image003.png","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
     })
         .then(() => idb.set({ key: 'cacheUpdatedAt', value: new Date().toISOString() }))
         .then(() => {
